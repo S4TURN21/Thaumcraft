0e06b87c03486421df2dbbb48cca122d32e674c0
S4TURN <s4turn21@gmail.com>
2023-08-08 13:37:40 +0300

    Initial commit

---
+++ src/main/java/thaumcraft/Thaumcraft.java
@@ -0,0 +1,13 @@
+package thaumcraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Thaumcraft.MODID)
+public class Thaumcraft
+{
+    public static final String MODID = "thaumcraft";
+    public Thaumcraft()
+    {
+
+    }
+}
