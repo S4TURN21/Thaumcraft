e79adebae519ee946f25ff3d518a3990cb4c69f7
S4TURN <s4turn21@gmail.com>
2023-08-09 01:38:36 +0300

    feat: register ColourHandlers

--- src/main/java/thaumcraft/proxies/ClientProxy.java
+++ src/main/java/thaumcraft/proxies/ClientProxy.java
@@ -4,5 +4,9 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import thaumcraft.client.ColorHandler;
 
 public class ClientProxy extends CommonProxy {
-
+    @Override
+    public void init(FMLCommonSetupEvent event) {
+        super.init(event);
+        ColorHandler.registerColourHandlers();
+    }
 }
