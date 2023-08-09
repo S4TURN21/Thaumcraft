8f399b7d5362423764df0157a3e3c6bae3c1095f
S4TURN <s4turn21@gmail.com>
2023-08-09 16:25:55 +0300

    feat: add BakeBlockEventHandler for ProxyBlock

--- src/main/java/thaumcraft/proxies/ProxyBlock.java
+++ src/main/java/thaumcraft/proxies/ProxyBlock.java
@@ -1,4 +1,13 @@
 package thaumcraft.proxies;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+import static thaumcraft.Thaumcraft.MODID;
+
 public class ProxyBlock {
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class BakeBlockEventHandler {
+
+    }
 }
