eb222ecd7bc45bc25e27e2c9a178d1ffb17f119b
S4TURN <s4turn21@gmail.com>
2023-08-09 16:27:05 +0300

    feat: add onRegisterGeometryLoaders for BakeBlockEventHandler

--- src/main/java/thaumcraft/proxies/ProxyBlock.java
+++ src/main/java/thaumcraft/proxies/ProxyBlock.java
@@ -1,13 +1,18 @@
 package thaumcraft.proxies;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ModelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import thaumcraft.client.renderers.block.CrystalModel;
 
 import static thaumcraft.Thaumcraft.MODID;
 
 public class ProxyBlock {
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class BakeBlockEventHandler {
-
+        @SubscribeEvent
+        public static void onRegisterGeometryLoaders(ModelEvent.RegisterGeometryLoaders event) {
+        }
     }
 }
