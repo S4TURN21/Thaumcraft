e45b2cf0f5094aa95be44ff0fbbe38c221bcf2e7
S4TURN <s4turn21@gmail.com>
2023-08-09 11:42:55 +0300

    feat: add crystal for SoundsTC

--- src/main/java/thaumcraft/common/lib/SoundsTC.java
+++ src/main/java/thaumcraft/common/lib/SoundsTC.java
@@ -2,14 +2,19 @@ package thaumcraft.common.lib;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraftforge.registries.RegisterEvent;
 
 public class SoundsTC {
+    public static SoundEvent crystal;
+    public static SoundType CRYSTAL;
+
     public static void registerSoundTypes() {
+        SoundsTC.CRYSTAL = new SoundType(0.5f, 1.0f, SoundsTC.crystal, SoundsTC.crystal, SoundsTC.crystal, SoundsTC.crystal, SoundsTC.crystal);
     }
 
     public static void registerSounds(RegisterEvent.RegisterHelper<SoundEvent> event) {
-
+        SoundsTC.crystal = getRegisteredSoundEvent(event, "thaumcraft:crystal");
     }
 
     private static SoundEvent getRegisteredSoundEvent(RegisterEvent.RegisterHelper<SoundEvent> event, String id) {
