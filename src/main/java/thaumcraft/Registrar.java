0bebfa4bd99912230ce029cfa26892bd98df3843
S4TURN <s4turn21@gmail.com>
2023-08-09 11:42:16 +0300

    feat: add register sounds for Registrar

--- src/main/java/thaumcraft/Registrar.java
+++ src/main/java/thaumcraft/Registrar.java
@@ -1,20 +1,28 @@
 package thaumcraft;
 
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegisterEvent;
 import thaumcraft.common.config.ConfigItems;
+import thaumcraft.common.lib.SoundsTC;
 
 @Mod.EventBusSubscriber(modid = Thaumcraft.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Registrar {
     @SubscribeEvent
     public static void onRegisterEvent(RegisterEvent event) {
         event.register(ForgeRegistries.Keys.ITEMS, Registrar::registerItems);
+        event.register(ForgeRegistries.Keys.SOUND_EVENTS, Registrar::registerSounds);
     }
 
     private static void registerItems(RegisterEvent.RegisterHelper<Item> event) {
         ConfigItems.initItems(event);
+    }
+
+    private static void registerSounds(RegisterEvent.RegisterHelper<SoundEvent> event) {
+        SoundsTC.registerSounds(event);
+        SoundsTC.registerSoundTypes();
     }
 }
