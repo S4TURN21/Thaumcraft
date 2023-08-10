e036633789ee2b417df37e1f751bb92c1f15e6b8
S4TURN <s4turn21@gmail.com>
2023-08-11 01:57:51 +0300

    feat: add register ModConfig

--- src/main/java/thaumcraft/Thaumcraft.java
+++ src/main/java/thaumcraft/Thaumcraft.java
@@ -2,8 +2,11 @@ package thaumcraft;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import thaumcraft.common.config.ModConfig;
+import thaumcraft.common.world.ThaumcraftWorldGenerator;
 import thaumcraft.proxies.ClientProxy;
 import thaumcraft.proxies.IProxy;
 import thaumcraft.proxies.ServerProxy;
@@ -15,6 +18,9 @@ public class Thaumcraft
     public static IProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
     public Thaumcraft()
     {
+        ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        ModConfig.register(modLoadingContext);
+
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(Thaumcraft.proxy::init);
     }
