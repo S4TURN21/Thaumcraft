32ae1e89eac556400a6117f8b20d83faee3c48bf
S4TURN <s4turn21@gmail.com>
2023-08-10 18:09:23 +0300

    feat(api): add makeCrystal for ThaumcraftApiHelper

--- src/main/java/thaumcraft/api/ThaumcraftApiHelper.java
+++ src/main/java/thaumcraft/api/ThaumcraftApiHelper.java
@@ -1,4 +1,22 @@
 package thaumcraft.api;
 
+import net.minecraft.world.item.ItemStack;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.items.ItemGenericEssentiaContainer;
+import thaumcraft.api.items.ItemsTC;
+
 public class ThaumcraftApiHelper {
+    public static ItemStack makeCrystal(Aspect aspect, int stackSize) {
+        if (aspect == null) {
+            return null;
+        }
+        ItemStack is = new ItemStack(ItemsTC.crystalEssence, stackSize);
+        ((ItemGenericEssentiaContainer) ItemsTC.crystalEssence).setAspects(is, new AspectList().add(aspect, 1));
+        return is;
+    }
+
+    public static ItemStack makeCrystal(Aspect aspect) {
+        return makeCrystal(aspect, 1);
+    }
 }
