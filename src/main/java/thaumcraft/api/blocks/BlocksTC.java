320d7cb2aad96aea412c64d13559a9539d91a500
S4TURN <s4turn21@gmail.com>
2023-08-10 21:26:09 +0300

    feat(api): add crystal block variants

--- src/main/java/thaumcraft/api/blocks/BlocksTC.java
+++ src/main/java/thaumcraft/api/blocks/BlocksTC.java
@@ -1,4 +1,13 @@
 package thaumcraft.api.blocks;
 
+import net.minecraft.world.level.block.Block;
+
 public class BlocksTC {
+    public static Block crystalAir;
+    public static Block crystalFire;
+    public static Block crystalWater;
+    public static Block crystalEarth;
+    public static Block crystalOrder;
+    public static Block crystalEntropy;
+    public static Block crystalTaint;
 }
