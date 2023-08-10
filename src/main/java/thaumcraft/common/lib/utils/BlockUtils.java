f6904bf92170f1535c9d5528c14d8ab82154cd5b
S4TURN <s4turn21@gmail.com>
2023-08-10 18:30:10 +0300

    feat: add isBlockTouching for BlockUtils

--- src/main/java/thaumcraft/common/lib/utils/BlockUtils.java
+++ src/main/java/thaumcraft/common/lib/utils/BlockUtils.java
@@ -1,4 +1,17 @@
 package thaumcraft.common.lib.utils;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.material.Material;
+
 public class BlockUtils {
+    public static boolean isBlockTouching(BlockGetter world, BlockPos pos, final Material mat, final boolean solid) {
+        for (final Direction face : Direction.values()) {
+            if (world.getBlockState(pos.relative(face)).getMaterial() == mat && (!solid || world.getBlockState(pos.relative(face)).isFaceSturdy(world, pos.relative(face), face.getOpposite()))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
