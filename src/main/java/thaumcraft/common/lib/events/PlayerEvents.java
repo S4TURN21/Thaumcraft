c27548c742c7325ec1935b934925d0917bff0389
S4TURN <s4turn21@gmail.com>
2023-08-11 19:42:41 +0300

    feat: add PlayerEvents

---
+++ src/main/java/thaumcraft/common/lib/events/PlayerEvents.java
@@ -0,0 +1,12 @@
+package thaumcraft.common.lib.events;
+
+import net.minecraftforge.fml.common.Mod;
+import thaumcraft.Thaumcraft;
+
+
+//TODO playerJoin sync knowledge
+//TODO cloneCapabilitiesEvent
+@Mod.EventBusSubscriber(modid = Thaumcraft.MODID)
+public class PlayerEvents {
+
+}
