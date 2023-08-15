a654b7cd818b645ee042bd2fa35436b7ebd83381
S4TURN <s4turn21@gmail.com>
2023-08-15 16:13:14 +0300

    feat: add Subscribe to AttachCapabilitiesEvent and RegisterCapabilitiesEvent

--- src/main/java/thaumcraft/common/lib/events/PlayerEvents.java
+++ src/main/java/thaumcraft/common/lib/events/PlayerEvents.java
@@ -1,7 +1,14 @@
 package thaumcraft.common.lib.events;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import thaumcraft.Thaumcraft;
+import thaumcraft.api.capabilities.IPlayerKnowledge;
+import thaumcraft.common.lib.capabilities.PlayerKnowledge;
 
 
 //TODO playerJoin sync knowledge
@@ -9,4 +16,15 @@ import thaumcraft.Thaumcraft;
 @Mod.EventBusSubscriber(modid = Thaumcraft.MODID)
 public class PlayerEvents {
 
+    @SubscribeEvent
+    public static void attachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof Player) {
+            event.addCapability(PlayerKnowledge.Provider.NAME, new PlayerKnowledge.Provider());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
+        event.register(IPlayerKnowledge.class);
+    }
 }
