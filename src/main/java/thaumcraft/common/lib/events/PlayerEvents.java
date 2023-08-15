f963e7098c7b198abeb7e2b5e5699842dc864618
S4TURN <s4turn21@gmail.com>
2023-08-15 16:39:28 +0300

    feat: add subscribe to EntityItemPickupEvent and PlayerWakeUpEvent

--- src/main/java/thaumcraft/common/lib/events/PlayerEvents.java
+++ src/main/java/thaumcraft/common/lib/events/PlayerEvents.java
@@ -1,20 +1,68 @@
 package thaumcraft.common.lib.events;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import thaumcraft.Thaumcraft;
 import thaumcraft.api.capabilities.IPlayerKnowledge;
+import thaumcraft.api.capabilities.ThaumcraftCapabilities;
+import thaumcraft.common.config.ConfigItems;
+import thaumcraft.common.config.ModConfig;
+import thaumcraft.common.items.resources.ItemCrystalEssence;
 import thaumcraft.common.lib.capabilities.PlayerKnowledge;
+import thaumcraft.common.lib.utils.InventoryUtils;
 
 
 //TODO playerJoin sync knowledge
 //TODO cloneCapabilitiesEvent
 @Mod.EventBusSubscriber(modid = Thaumcraft.MODID)
 public class PlayerEvents {
+    @SubscribeEvent
+    public static void pickupItem(EntityItemPickupEvent event) {
+        if (event.getEntity() != null && !event.getItem().level.isClientSide && event.getItem() != null && event.getItem().getItem() != null) {
+            IPlayerKnowledge knowledge = ThaumcraftCapabilities.getKnowledge(event.getEntity());
+            if (event.getItem().getItem().getItem() instanceof ItemCrystalEssence && !knowledge.isResearchKnown("!gotcrystals")) {
+                knowledge.addResearch("!gotcrystals");
+                knowledge.sync((ServerPlayer) event.getEntity());
+                event.getEntity().sendSystemMessage(Component.translatable("got.crystals").withStyle(ChatFormatting.DARK_PURPLE));
+                if (ModConfig.CONFIG_MISC.noSleep.get() && !knowledge.isResearchKnown("!gotdream")) {
+                    giveDreamJournal(event.getEntity());
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void wakeUp(PlayerWakeUpEvent event) {
+        IPlayerKnowledge knowledge = ThaumcraftCapabilities.getKnowledge(event.getEntity());
+        if (event.getEntity() != null && !event.getEntity().level.isClientSide && knowledge.isResearchKnown("!gotcrystals") && !knowledge.isResearchKnown("!gotdream")) {
+            giveDreamJournal(event.getEntity());
+        }
+    }
+
+    private static void giveDreamJournal(Player player) {
+        final IPlayerKnowledge knowledge = ThaumcraftCapabilities.getKnowledge(player);
+        knowledge.addResearch("!gotdream");
+        knowledge.sync((ServerPlayer) player);
+        ItemStack book = ConfigItems.startBook.copy();
+        book.getTag().putString("author", player.getName().getString());
+        if (!player.getInventory().add(book)) {
+            InventoryUtils.dropItemAtEntity(player.level, book, player);
+        }
+        try {
+            player.sendSystemMessage(Component.translatable("got.dream").withStyle(ChatFormatting.DARK_PURPLE));
+        } catch (Exception ex) {
+        }
+    }
 
     @SubscribeEvent
     public static void attachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
