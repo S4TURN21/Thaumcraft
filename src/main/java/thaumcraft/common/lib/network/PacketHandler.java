a4eec2ec7f0d8a6093738dc4fd7a6c5f10dc6ec0
S4TURN <s4turn21@gmail.com>
2023-08-15 16:07:45 +0300

    feat: add register PacketSyncKnowledge for PacketHandler

--- src/main/java/thaumcraft/common/lib/network/PacketHandler.java
+++ src/main/java/thaumcraft/common/lib/network/PacketHandler.java
@@ -1,9 +1,13 @@
 package thaumcraft.common.lib.network;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 import thaumcraft.Thaumcraft;
+import thaumcraft.common.lib.network.playerdata.PacketSyncKnowledge;
+
+import java.util.Optional;
 
 public class PacketHandler {
     private static final String PROTOCOL_VERSION = "1.0";
@@ -14,5 +18,6 @@ public class PacketHandler {
 
     public static void preInit() {
         int idx = 0;
+        PacketHandler.INSTANCE.registerMessage(idx, PacketSyncKnowledge.class, PacketSyncKnowledge::toBytes, PacketSyncKnowledge::fromBytes, PacketSyncKnowledge::onMessage, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
     }
 }
