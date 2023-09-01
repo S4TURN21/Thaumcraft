b6c855634a44521d2ef82363f23634a92a60278a
S4TURN <s4turn21@gmail.com>
2023-09-01 15:22:16 +0300

    feat(api): add knowsResearch, knowsResearchStrict

--- src/main/java/thaumcraft/api/capabilities/ThaumcraftCapabilities.java
+++ src/main/java/thaumcraft/api/capabilities/ThaumcraftCapabilities.java
@@ -1,6 +1,5 @@
 package thaumcraft.api.capabilities;
 
-import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -13,5 +12,53 @@ public class ThaumcraftCapabilities {
 
     public static IPlayerKnowledge getKnowledge(@NotNull Player player) {
         return player.getCapability(ThaumcraftCapabilities.KNOWLEDGE, null).resolve().get();
+    }
+
+    public static boolean knowsResearch(Player player, String... research) {
+        for (final String r : research) {
+            if (r.contains("&&")) {
+                final String[] rr = r.split("&&");
+                if (!knowsResearch(player, rr)) {
+                    return false;
+                }
+            } else if (r.contains("||")) {
+                final String[] split;
+                final String[] rr = split = r.split("||");
+                for (final String str : split) {
+                    if (knowsResearch(player, str)) {
+                        return true;
+                    }
+                }
+            } else if (!getKnowledge(player).isResearchKnown(r)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean knowsResearchStrict(Player player, String... research) {
+        for (final String r : research) {
+            if (r.contains("&&")) {
+                final String[] rr = r.split("&&");
+                if (!knowsResearchStrict(player, rr)) {
+                    return false;
+                }
+            } else if (r.contains("||")) {
+                final String[] split;
+                final String[] rr = split = r.split("||");
+                for (final String str : split) {
+                    if (knowsResearchStrict(player, str)) {
+                        return true;
+                    }
+                }
+            } else if (r.contains("@")) {
+                if (!getKnowledge(player).isResearchKnown(r)) {
+                    return false;
+                }
+            } else if (!getKnowledge(player).isResearchComplete(r)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
