de76aec3890952b671b3346ebb8f79918341e7b3
S4TURN <s4turn21@gmail.com>
2023-09-01 13:50:12 +0300

    feat(api): add CommonInternals

---
+++ src/main/java/thaumcraft/api/internal/CommonInternals.java
@@ -0,0 +1,9 @@
+package thaumcraft.api.internal;
+
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.HashMap;
+
+public class CommonInternals {
+    public static HashMap<String, ResourceLocation> jsonLocs = new HashMap<>();
+}
