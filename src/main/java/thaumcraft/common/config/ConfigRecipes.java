36c97dd1f628d5e5baca31ac3c66a39e741f6106
S4TURN <s4turn21@gmail.com>
2023-09-16 18:51:49 +0300

    feat: add initializeCompoundRecipes

--- src/main/java/thaumcraft/common/config/ConfigRecipes.java
+++ src/main/java/thaumcraft/common/config/ConfigRecipes.java
@@ -1,10 +1,19 @@
 package thaumcraft.common.config;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.RegisterEvent;
+import thaumcraft.api.crafting.IDustTrigger;
+import thaumcraft.api.items.ItemsTC;
+import thaumcraft.common.lib.crafting.DustTriggerSimple;
 import thaumcraft.common.lib.crafting.RecipeMagicDust;
 
 public class ConfigRecipes {
+    public static void initializeCompoundRecipes() {
+        IDustTrigger.registerDustTrigger(new DustTriggerSimple("!gotdream", Blocks.BOOKSHELF, new ItemStack(ItemsTC.thaumonomicon)));
+    }
+
     public static void initializeNormalRecipes(RegisterEvent.RegisterHelper<RecipeSerializer<?>> event) {
         event.register("salismundus", RecipeMagicDust.Serializer.INSTANCE);
     }
