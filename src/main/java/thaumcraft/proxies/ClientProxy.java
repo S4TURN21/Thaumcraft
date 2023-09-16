e30f290ac3df0389b0e9d1741b5f2335af11bde5
S4TURN <s4turn21@gmail.com>
2023-09-16 23:17:56 +0300

    feat: add call setupProxyEntityRenderers for ClientProxy

--- src/main/java/thaumcraft/proxies/ClientProxy.java
+++ src/main/java/thaumcraft/proxies/ClientProxy.java
@@ -4,9 +4,16 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import thaumcraft.client.ColorHandler;
 
 public class ClientProxy extends CommonProxy {
+    ProxyEntities proxyEntities;
+
+    public ClientProxy() {
+        this.proxyEntities = new ProxyEntities();
+    }
+
     @Override
     public void init(FMLCommonSetupEvent event) {
         super.init(event);
         ColorHandler.registerColourHandlers();
+        this.proxyEntities.setupEntityRenderers();
     }
 }
