f600aca9152c2ffdb7347c405e50a90ba45fec68
S4TURN <s4turn21@gmail.com>
2023-09-16 20:32:58 +0300

    feat: add ConfigEntities

---
+++ src/main/java/thaumcraft/common/config/ConfigEntities.java
@@ -0,0 +1,13 @@
+package thaumcraft.common.config;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.registries.RegisterEvent;
+import thaumcraft.common.entities.EntitySpecialItem;
+
+public class ConfigEntities {
+    public static void initEntities(RegisterEvent.RegisterHelper<EntityType<?>> iForgeRegistry) {
+        iForgeRegistry.register(new ResourceLocation("thaumcraft", "special_item"), EntityType.Builder.<EntitySpecialItem>of(EntitySpecialItem::new, MobCategory.MISC).clientTrackingRange(64).updateInterval(20).setShouldReceiveVelocityUpdates(true).noSummon().build("special_item"));
+    }
+}
