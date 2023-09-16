8a61083209c9cdc450fbd032c1a64012dba6b8ca
S4TURN <s4turn21@gmail.com>
2023-09-16 18:48:46 +0300

    feat: add InventoryFake

---
+++ src/main/java/thaumcraft/common/InventoryFake.java
@@ -0,0 +1,9 @@
+package thaumcraft.common;
+
+import net.minecraft.world.SimpleContainer;
+
+public class InventoryFake extends SimpleContainer {
+    public InventoryFake(int size) {
+        super(size);
+    }
+}
