7819d52940a7fa38bdd958f79b18f9dbfbc610ac
S4TURN <s4turn21@gmail.com>
2023-09-20 22:44:37 +0300

    feat(api): add getText, setText, getWarp, setWarp for ResearchStage

--- src/main/java/thaumcraft/api/research/ResearchStage.java
+++ src/main/java/thaumcraft/api/research/ResearchStage.java
@@ -1,4 +1,22 @@
 package thaumcraft.api.research;
 
 public class ResearchStage {
+    String text;
+    int warp;
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getWarp() {
+        return this.warp;
+    }
+
+    public void setWarp(int warp) {
+        this.warp = warp;
+    }
 }
