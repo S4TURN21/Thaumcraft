78a2dfeb16c8613b4f4611fdf71faeecaa33d865
S4TURN <s4turn21@gmail.com>
2024-03-04 01:03:29 +0300

    feat: add IThaumcraftRecipe

---
+++ src/main/java/thaumcraft/api/crafting/IThaumcraftRecipe.java
@@ -0,0 +1,7 @@
+package thaumcraft.api.crafting;
+
+public interface IThaumcraftRecipe {
+    String getResearch();
+
+    String getGroup();
+}
--- src/main/java/thaumcraft/proxies/CommonProxy.java
+++ src/main/java/thaumcraft/proxies/CommonProxy.java
@@ -1,12 +1,15 @@
 package thaumcraft.proxies;
 
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.common.config.ConfigResearch;
+import thaumcraft.common.lib.InternalMethodHandler;
 import thaumcraft.common.lib.network.PacketHandler;
 
 public class CommonProxy implements IProxy {
     @Override
     public void preInit(FMLCommonSetupEvent event) {
+        ThaumcraftApi.internalMethods = new InternalMethodHandler();
         PacketHandler.preInit();
     }
 
