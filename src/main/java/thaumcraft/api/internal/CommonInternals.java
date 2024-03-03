c58c60f1c222f8bac01337c43f948216a9a3def3
S4TURN <s4turn21@gmail.com>
2024-03-04 01:02:07 +0300

    feat: add craftingRecipeCatalog for CommonInternals

--- src/main/java/thaumcraft/api/internal/CommonInternals.java
+++ src/main/java/thaumcraft/api/internal/CommonInternals.java
@@ -1,9 +1,20 @@
 package thaumcraft.api.internal;
 
 import net.minecraft.resources.ResourceLocation;
+import thaumcraft.api.crafting.IThaumcraftRecipe;
 
 import java.util.HashMap;
 
 public class CommonInternals {
     public static HashMap<String, ResourceLocation> jsonLocs = new HashMap<>();
+    public static HashMap<ResourceLocation, IThaumcraftRecipe> craftingRecipeCatalog = new HashMap<>();
+    public static HashMap<ResourceLocation, Object> craftingRecipeCatalogFake = new HashMap<>();
+
+    public static IThaumcraftRecipe getCatalogRecipe(ResourceLocation key) {
+        return CommonInternals.craftingRecipeCatalog.get(key);
+    }
+
+    public static Object getCatalogRecipeFake(ResourceLocation key) {
+        return CommonInternals.craftingRecipeCatalogFake.get(key);
+    }
 }
