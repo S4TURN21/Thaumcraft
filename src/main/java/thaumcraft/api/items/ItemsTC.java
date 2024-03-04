c4af6636201c4c7a1e6e7f5192e6348eafcfe748
S4TURN <s4turn21@gmail.com>
2024-03-04 16:29:29 +0300

    feat: add enchantedPlaceholder

--- src/main/java/thaumcraft/api/items/ItemsTC.java
+++ src/main/java/thaumcraft/api/items/ItemsTC.java
@@ -6,4 +6,5 @@ public class ItemsTC {
     public static Item thaumonomicon;
     public static Item crystalEssence;
     public static Item salisMundus;
+    public static Item enchantedPlaceholder;
 }
