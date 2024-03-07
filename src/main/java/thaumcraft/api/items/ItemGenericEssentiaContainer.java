531c33e3f0000d7fd2d171b937057a2daff7e0f6
S4TURN <s4turn21@gmail.com>
2024-03-07 23:59:34 +0300

    feat: add inventoryTick, onCraftedBy for ItemGenericEssentiaContainer

--- src/main/java/thaumcraft/api/items/ItemGenericEssentiaContainer.java
+++ src/main/java/thaumcraft/api/items/ItemGenericEssentiaContainer.java
@@ -1,8 +1,12 @@
 package thaumcraft.api.items;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 
 public class ItemGenericEssentiaContainer extends Item {
@@ -24,5 +28,22 @@ public class ItemGenericEssentiaContainer extends Item {
             itemstack.setTag(new CompoundTag());
         }
         aspects.writeToNBT(itemstack.getTag());
+    }
+
+    @Override
+    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+        if (!pLevel.isClientSide && !pStack.hasTag()) {
+            Aspect[] displayAspects = Aspect.aspects.values().toArray(new Aspect[]{});
+            setAspects(pStack, new AspectList().add(displayAspects[pLevel.random.nextInt(displayAspects.length)], base));
+        }
+        super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
+    }
+
+    @Override
+    public void onCraftedBy(ItemStack pStack, Level pLevel, Player pPlayer) {
+        if (!pLevel.isClientSide && !pStack.hasTag()) {
+            Aspect[] displayAspects = Aspect.aspects.values().toArray(new Aspect[]{});
+            setAspects(pStack, new AspectList().add(displayAspects[pLevel.random.nextInt(displayAspects.length)], base));
+        }
     }
 }
