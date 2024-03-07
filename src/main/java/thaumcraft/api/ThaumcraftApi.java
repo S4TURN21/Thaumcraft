78b962eeb6a615e7394ebe681b0678c3cf6adc29
S4TURN <s4turn21@gmail.com>
2024-03-07 23:57:01 +0300

    feat: add salismundusfake

--- src/main/java/thaumcraft/api/ThaumcraftApi.java
+++ src/main/java/thaumcraft/api/ThaumcraftApi.java
@@ -5,6 +5,8 @@ import thaumcraft.api.internal.CommonInternals;
 import thaumcraft.api.internal.DummyInternalMethodHandler;
 import thaumcraft.api.internal.IInternalMethodHandler;
 
+import java.util.HashMap;
+
 public class ThaumcraftApi {
     public static IInternalMethodHandler internalMethods = new DummyInternalMethodHandler();
 
@@ -12,5 +14,13 @@ public class ThaumcraftApi {
         if (!CommonInternals.jsonLocs.containsKey(loc.toString())) {
             CommonInternals.jsonLocs.put(loc.toString(), loc);
         }
+    }
+
+    public static HashMap<ResourceLocation, Object> getCraftingRecipesFake() {
+        return CommonInternals.craftingRecipeCatalogFake;
+    }
+
+    public static void addFakeCraftingRecipe(ResourceLocation registry, Object recipe) {
+        getCraftingRecipesFake().put(registry, recipe);
     }
 }
--- src/main/java/thaumcraft/common/config/ConfigRecipes.java
+++ src/main/java/thaumcraft/common/config/ConfigRecipes.java
@@ -1,20 +1,33 @@
 package thaumcraft.common.config;
 
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.ShapelessRecipe;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.RegisterEvent;
+import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.crafting.IDustTrigger;
 import thaumcraft.api.items.ItemsTC;
 import thaumcraft.common.lib.crafting.DustTriggerSimple;
 import thaumcraft.common.lib.crafting.RecipeMagicDust;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ConfigRecipes {
+    static ResourceLocation defaultGroup = new ResourceLocation("");
+    public static HashMap<String, ArrayList<ResourceLocation>> recipeGroups = new HashMap<>();
+
     public static void initializeCompoundRecipes() {
         IDustTrigger.registerDustTrigger(new DustTriggerSimple("!gotdream", Blocks.BOOKSHELF, new ItemStack(ItemsTC.thaumonomicon)));
     }
 
     public static void initializeNormalRecipes(RegisterEvent.RegisterHelper<RecipeSerializer<?>> event) {
         event.register("salismundus", RecipeMagicDust.Serializer.INSTANCE);
+        ThaumcraftApi.addFakeCraftingRecipe(new ResourceLocation("thaumcraft:salismundusfake"), new ShapelessRecipe(ConfigRecipes.defaultGroup, "", new ItemStack(ItemsTC.salisMundus), NonNullList.of(Ingredient.EMPTY, Ingredient.of(Items.FLINT), Ingredient.of(Items.BOWL), Ingredient.of(Items.REDSTONE), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)))));
     }
 }
