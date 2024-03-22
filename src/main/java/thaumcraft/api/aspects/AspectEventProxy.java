7fd02296bee85aaf54e093bba0c57876acd3936f
S4TURN <s4turn21@gmail.com>
2024-03-23 02:05:38 +0300

    feat: add AspectEventProxy

---
+++ src/main/java/thaumcraft/api/aspects/AspectEventProxy.java
@@ -0,0 +1,34 @@
+package thaumcraft.api.aspects;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITagManager;
+import thaumcraft.api.internal.CommonInternals;
+
+public class AspectEventProxy {
+    public void registerObjectTag(ItemStack item, AspectList aspects) {
+        if (aspects == null) aspects = new AspectList();
+        try {
+            CommonInternals.objectTags.put(CommonInternals.generateUniqueItemstackId(item), aspects);
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void registerObjectTag(TagKey<Item> oreDict, AspectList aspects) {
+        if (aspects == null) aspects = new AspectList();
+
+        ITagManager<Item> ores = ForgeRegistries.ITEMS.tags();
+        if (ores != null) {
+            for (ItemStack ore : ores.getTag(oreDict).stream().map(ItemStack::new).toList()) {
+                try {
+                    ItemStack oc = ore.copy();
+                    oc.setCount(1);
+                    registerObjectTag(oc, aspects.copy());
+                } catch (Exception ignored) {
+                }
+            }
+        }
+    }
+}
