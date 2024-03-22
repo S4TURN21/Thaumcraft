8e96087928463d96b402cd7cc3641cb538812414
S4TURN <s4turn21@gmail.com>
2024-03-22 16:00:15 +0300

    feat: add generateUniqueItemstackId

--- src/main/java/thaumcraft/api/internal/CommonInternals.java
+++ src/main/java/thaumcraft/api/internal/CommonInternals.java
@@ -1,6 +1,7 @@
 package thaumcraft.api.internal;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import thaumcraft.api.crafting.IThaumcraftRecipe;
 
 import java.util.HashMap;
@@ -16,5 +17,12 @@ public class CommonInternals {
 
     public static Object getCatalogRecipeFake(ResourceLocation key) {
         return CommonInternals.craftingRecipeCatalogFake.get(key);
+    }
+
+    public static int generateUniqueItemstackId(ItemStack stack) {
+        ItemStack sc = stack.copy();
+        sc.setCount(1);
+        String ss = sc.serializeNBT().toString();
+        return ss.hashCode();
     }
 }
