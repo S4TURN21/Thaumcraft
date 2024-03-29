83f850bade2a4e594fda76040a8c178ddb26be4d
S4TURN <s4turn21@gmail.com>
2024-03-29 20:34:09 +0300

    feat: add default group to recipes

--- src/main/java/thaumcraft/common/config/ConfigRecipes.java
+++ src/main/java/thaumcraft/common/config/ConfigRecipes.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 import java.util.function.Consumer;
 
 public class ConfigRecipes extends RecipeProvider {
-    static ResourceLocation defaultGroup = new ResourceLocation("");
+    static String defaultGroup = "";
     public static HashMap<String, ArrayList<ResourceLocation>> recipeGroups = new HashMap<>();
 
     public ConfigRecipes(DataGenerator pGenerator) {
@@ -40,7 +40,7 @@ public class ConfigRecipes extends RecipeProvider {
     }
 
     public static void initializeFakeRecipes() {
-        ThaumcraftApi.addFakeCraftingRecipe(new ResourceLocation("thaumcraft:salismundusfake"), new ShapelessRecipe(ConfigRecipes.defaultGroup, "", new ItemStack(ItemsTC.salisMundus), NonNullList.of(Ingredient.EMPTY, Ingredient.of(Items.FLINT), Ingredient.of(Items.BOWL), Ingredient.of(Items.REDSTONE), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)))));
+        ThaumcraftApi.addFakeCraftingRecipe(new ResourceLocation("thaumcraft:salismundusfake"), new ShapelessRecipe(new ResourceLocation("thaumcraft:salismundusfake"), ConfigRecipes.defaultGroup, new ItemStack(ItemsTC.salisMundus), NonNullList.of(Ingredient.EMPTY, Ingredient.of(Items.FLINT), Ingredient.of(Items.BOWL), Ingredient.of(Items.REDSTONE), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)), Ingredient.of(new ItemStack(ItemsTC.crystalEssence, 1)))));
     }
 
     @Override
@@ -50,22 +50,12 @@ public class ConfigRecipes extends RecipeProvider {
     }
 
     public static void initializeArcaneRecipes(Consumer<FinishedRecipe> pFinishedRecipeConsumer) {
-        ShapedArcaneRecipeBuilder.shaped(ItemsTC.thaumometer)
-                .research("FIRSTSTEPS@2")
-                .vis(20)
-                .crystals(new AspectList().add(Aspect.AIR, 1).add(Aspect.EARTH, 1).add(Aspect.WATER, 1).add(Aspect.FIRE, 1).add(Aspect.ORDER, 1).add(Aspect.ENTROPY, 1))
-                .pattern(" I ")
-                .pattern("IGI")
-                .pattern(" I ")
-                .define('I', Items.GOLD_INGOT)
-                .define('G', Blocks.GLASS_PANE)
-                .unlockedBy("has_arcane_workbench", has(BlocksTC.arcaneWorkbench))
-                .save(pFinishedRecipeConsumer);
+        ShapedArcaneRecipeBuilder.shaped(ItemsTC.thaumometer).group(ConfigRecipes.defaultGroup).research("FIRSTSTEPS@2").vis(20).crystals(new AspectList().add(Aspect.AIR, 1).add(Aspect.EARTH, 1).add(Aspect.WATER, 1).add(Aspect.FIRE, 1).add(Aspect.ORDER, 1).add(Aspect.ENTROPY, 1)).pattern(" I ").pattern("IGI").pattern(" I ").define('I', Items.GOLD_INGOT).define('G', Blocks.GLASS_PANE).unlockedBy("has_arcane_workbench", has(BlocksTC.arcaneWorkbench)).save(pFinishedRecipeConsumer);
     }
 
     public static void initializeNormalRecipes(Consumer<FinishedRecipe> pFinishedRecipeConsumer) {
-        ShapedRecipeBuilder.shaped(BlocksTC.stoneArcane, 9).pattern("KKK").pattern("KCK").pattern("KKK").define('K', Items.STONE).define('C', ItemsTC.crystalEssence).unlockedBy("has_crystal", has(ItemsTC.crystalEssence)).save(pFinishedRecipeConsumer);
-        ShapedRecipeBuilder.shaped(BlocksTC.stoneArcaneBrick, 4).pattern("KK").pattern("KK").define('K', BlocksTC.stoneArcane).unlockedBy("has_stone_arcane", has(BlocksTC.stoneArcane)).save(pFinishedRecipeConsumer);
-        ShapedRecipeBuilder.shaped(BlocksTC.tableWood).pattern("SSS").pattern("W W").define('S', ItemTags.WOODEN_SLABS).define('W', ItemTags.PLANKS).unlockedBy("has_table_wood", has(BlocksTC.tableWood)).save(pFinishedRecipeConsumer);
+        ShapedRecipeBuilder.shaped(BlocksTC.stoneArcane, 9).group(ConfigRecipes.defaultGroup).pattern("KKK").pattern("KCK").pattern("KKK").define('K', Items.STONE).define('C', ItemsTC.crystalEssence).unlockedBy("has_crystal", has(ItemsTC.crystalEssence)).save(pFinishedRecipeConsumer);
+        ShapedRecipeBuilder.shaped(BlocksTC.stoneArcaneBrick, 4).group(ConfigRecipes.defaultGroup).pattern("KK").pattern("KK").define('K', BlocksTC.stoneArcane).unlockedBy("has_stone_arcane", has(BlocksTC.stoneArcane)).save(pFinishedRecipeConsumer);
+        ShapedRecipeBuilder.shaped(BlocksTC.tableWood).group(ConfigRecipes.defaultGroup).pattern("SSS").pattern("W W").define('S', ItemTags.WOODEN_SLABS).define('W', ItemTags.PLANKS).unlockedBy("has_table_wood", has(BlocksTC.tableWood)).save(pFinishedRecipeConsumer);
     }
 }
