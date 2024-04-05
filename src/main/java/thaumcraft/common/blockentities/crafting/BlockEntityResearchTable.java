6a3ac6149848f64c4170dad47e85685befecaaef
S4TURN <s4turn21@gmail.com>
2024-04-05 12:27:49 +0300

    feat: add startNewTheory, checkSurroundingAids

--- src/main/java/thaumcraft/common/blockentities/crafting/BlockEntityResearchTable.java
+++ src/main/java/thaumcraft/common/blockentities/crafting/BlockEntityResearchTable.java
@@ -7,18 +7,27 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
 import thaumcraft.api.items.IScribeTools;
+import thaumcraft.api.research.theorycraft.ITheorycraftAid;
 import thaumcraft.api.research.theorycraft.ResearchTableData;
+import thaumcraft.api.research.theorycraft.TheorycraftManager;
 import thaumcraft.common.blockentities.BlockEntityThaumcraftInventory;
 import thaumcraft.common.container.ContainerResearchTable;
+import thaumcraft.common.lib.utils.EntityUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 public class BlockEntityResearchTable extends BlockEntityThaumcraftInventory implements MenuProvider {
     public ResearchTableData data;
@@ -47,6 +56,54 @@ public class BlockEntityResearchTable extends BlockEntityThaumcraftInventory imp
             nbttagcompound.remove("note");
         }
         return super.writeSyncNBT(nbttagcompound);
+    }
+
+    public void startNewTheory(Player player, Set<String> mutators) {
+        (data = new ResearchTableData(player, this)).initialize(player, mutators);
+        syncTile(false);
+        setChanged();
+    }
+
+    public Set<String> checkSurroundingAids() {
+        HashMap<String, ITheorycraftAid> mutators = new HashMap<>();
+        for (int y = -1; y <= 1; ++y) {
+            for (int x = -4; x <= 4; ++x) {
+                for (int z = -4; z <= 4; ++z) {
+                    for (String muk : TheorycraftManager.aids.keySet()) {
+                        ITheorycraftAid mu = TheorycraftManager.aids.get(muk);
+                        BlockState state = level.getBlockState(getBlockPos().offset(x, y, z));
+                        if (mu.getAidObject() instanceof Block) {
+                            if (state.getBlock() != mu.getAidObject()) {
+                                continue;
+                            }
+                            mutators.put(muk, mu);
+                        } else {
+                            if (!(mu.getAidObject() instanceof ItemStack)) {
+                                continue;
+                            }
+                            ItemStack is = state.getBlock().getCloneItemStack(getLevel(), getBlockPos().offset(x, y, z), state);
+
+                            if (is == null || is.isEmpty() || !ItemStack.isSameIgnoreDurability(is, (ItemStack)mu.getAidObject())) {
+                                continue;
+                            }
+                            mutators.put(muk, mu);
+                        }
+                    }
+                }
+            }
+        }
+        List<Entity> l = EntityUtils.getEntitiesInRange(getLevel(), getBlockPos(), null, Entity.class, 5.0);
+        if (l != null && !l.isEmpty()) {
+            for (Entity e : l) {
+                for (String muk : TheorycraftManager.aids.keySet()) {
+                    ITheorycraftAid mu = TheorycraftManager.aids.get(muk);
+                    if (mu.getAidObject() instanceof Class && e.getClass().isAssignableFrom((Class<?>) mu.getAidObject())) {
+                        mutators.put(muk, mu);
+                    }
+                }
+            }
+        }
+        return mutators.keySet();
     }
 
     @Override
