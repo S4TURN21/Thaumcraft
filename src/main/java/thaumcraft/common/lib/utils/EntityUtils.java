404f8c8927bfa8446ac62873b45db8f6739635e9
S4TURN <s4turn21@gmail.com>
2025-03-17 03:00:52 +0300

    feat: add hasGoggles, showPopups

--- src/main/java/thaumcraft/common/lib/utils/EntityUtils.java
+++ src/main/java/thaumcraft/common/lib/utils/EntityUtils.java
@@ -4,9 +4,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import thaumcraft.api.items.IGoggles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +27,26 @@ public class EntityUtils {
         py += vec3d.y * 0.3;
         pz += vec3d.z * 0.3;
         return new Vec3(px, py, pz);
+    }
+
+    public static boolean hasGoggles(Entity e) {
+        if (!(e instanceof Player)) {
+            return false;
+        }
+        Player viewer = (Player) e;
+        if (viewer.getMainHandItem().getItem() instanceof IGoggles && showPopups(viewer.getMainHandItem(), viewer)) {
+            return true;
+        }
+        for (int a = 0; a < 4; ++a) {
+            if (viewer.getInventory().getArmor(a).getItem() instanceof IGoggles && showPopups(viewer.getInventory().getArmor(a), viewer)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean showPopups(ItemStack stack, Player player) {
+        return ((IGoggles) stack.getItem()).showIngamePopups(stack, player);
     }
 
     public static <T extends Entity> List<T> getEntitiesInRange(Level world, BlockPos pos, Entity entity, Class<? extends T> classEntity, double range) {
