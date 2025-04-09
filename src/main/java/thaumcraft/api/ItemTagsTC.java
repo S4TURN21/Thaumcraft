2c0cf81df0cdc53f97b88e727ac5994c0a71b774
S4TURN <s4turn21@gmail.com>
2025-04-09 18:11:27 +0300

    feat: add tag ingots brass

--- src/main/java/thaumcraft/api/ItemTagsTC.java
+++ src/main/java/thaumcraft/api/ItemTagsTC.java
@@ -9,10 +9,12 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
+import thaumcraft.api.items.ItemsTC;
 
 public class ItemTagsTC extends ItemTagsProvider {
     public static final TagKey<Item> SILVERWOOD_LOGS = ItemTags.create(new ResourceLocation("thaumcraft", "silverwood_logs"));
     public static final TagKey<Item> NITOR = ItemTags.create(new ResourceLocation("thaumcraft", "nitor"));
+    public static final TagKey<Item> INGOTS_BRASS = ItemTags.create(new ResourceLocation("thaumcraft", "ingots/brass"));
 
     public ItemTagsTC(DataGenerator generator, BlockTagsProvider blockTagsProvider, String modId, @Nullable ExistingFileHelper existingFileHelper) {
         super(generator, blockTagsProvider, modId, existingFileHelper);
@@ -22,5 +24,6 @@ public class ItemTagsTC extends ItemTagsProvider {
     protected void addTags() {
         this.copy(BlockTagsTC.SILVERWOOD_LOGS, ItemTagsTC.SILVERWOOD_LOGS);
         this.copy(BlockTagsTC.NITOR, ItemTagsTC.NITOR);
+        this.tag(INGOTS_BRASS).add(ItemsTC.brassIngot);
     }
 }
