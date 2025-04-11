50702656e365fc6806baae7efb2531ca30032f2e
S4TURN <s4turn21@gmail.com>
2025-04-11 15:20:40 +0300

    fix: update armor material special

--- src/main/java/thaumcraft/api/ThaumcraftMaterials.java
+++ src/main/java/thaumcraft/api/ThaumcraftMaterials.java
@@ -6,5 +6,5 @@ import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.crafting.Ingredient;
 
 public class ThaumcraftMaterials {
-    public static ArmorMaterial ARMORMAT_SPECIAL = new SimpleArmorMaterial(new int[]{13, 15, 16, 11}, new int[]{1, 2, 3, 1}, 25, SoundEvents.ARMOR_EQUIP_LEATHER, () -> Ingredient.EMPTY, "SPECIAL", 1.0F, 0.0f);
+    public static ArmorMaterial ARMORMAT_SPECIAL = SimpleArmorMaterial.builder().textureName("SPECIAL").durabilityFactor(25).damageReduction(new int[]{1, 2, 3, 1}).enchantability(25).soundEvent(SoundEvents.ARMOR_EQUIP_LEATHER).toughness(1.0F).repairMaterial(() -> Ingredient.EMPTY).build();
 }
